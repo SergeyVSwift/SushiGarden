a7072f82f3cdfec77b9db2d3cc54d801ae02f0a5
SergeyVSwift <vashhenkobs@gmail.com>
2024-07-26 17:28:22 +0300

    Initial Commit

---
+++ SushiGarden/SushiGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiGardenApp.swift
+//  SushiGarden
+//
+//  Created by Сергей Ващенко on 26.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SushiGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
