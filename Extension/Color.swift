6c0621db799ab754815c670c18c0c1546d1fca80
SergeyVSwift <vashhenkobs@gmail.com>
2024-07-27 10:41:54 +0300

    Добавлена авторизация с комбайном без логики

---
+++ Extension/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  SushiGarden
+//
+//  Created by Сергей Ващенко on 26.07.24.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static var darkW: Color {
+        return Color("Dark")
+    }
+    
+    static var WhiteW: Color {
+        return Color("White")
+    }
+    
+    static var bgColor: Color {
+        return Color("bgColor")
+    }
+}
--- SushiGarden/ContentView.swift
+++ SushiGarden/ContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.bgColor
+                .ignoresSafeArea()
+            
+            ScrollView(showsIndicators: false) {
+                Image("logoGreen")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width / 1.1, height: UIScreen.main.bounds.width / 1.1)
+                
+                VStack(spacing: 5) {
+                    
+                    Text("Authorization")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .fontWeight(.bold)
+                        .fontDesign(.rounded)
+                        .padding(8)
+                    
+                    VStack(spacing: 20) {
+                        CustomTextField(title: "Phone", text: $viewModel.phone, prompt: viewModel.phonePrompt)
+                            .onChange(of: viewModel.phone) { newValue in
+                                DispatchQueue.main.async {
+                                    viewModel.phone = viewModel.phone.formattedMask(text: viewModel.phone, mask: "+X (XXX) XXX-XX-XX")
+                                }
+                            }
+                        
+                        CustomTextField(title: "Password", text: $viewModel.password, prompt: viewModel.passwordPrompt, isSecure: true)
+                    }
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding()
+                    .padding(.bottom, 30)
+                    
+                    Button {} label: {
+                        ZStack {
+                            AnimatedGradient(colors: [Color.cyan, Color.purple])
+                            Text("Login")
+                                 .foregroundColor(.white)
+                                 .fontDesign(.rounded)
+                                 .fontWeight(.semibold)
+                        }
+                        .frame(width: 120, height: 45)
+                        .cornerRadius(10)
+                    }
+                    .disabled(!viewModel.canSubmit)
+
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
